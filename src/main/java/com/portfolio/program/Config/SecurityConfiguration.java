32069191bae02f5ad098e8b2b2752e9a46567de9
Casatti Ivan <casativan@gmail.com>
2023-02-09 18:49:09 -0300

    Cors Try 1.6

--- src/main/java/com/portfolio/program/Config/SecurityConfiguration.java
+++ src/main/java/com/portfolio/program/Config/SecurityConfiguration.java
@@ -34,7 +34,8 @@ public class SecurityConfiguration {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors();
 
         return http.build();
     }
